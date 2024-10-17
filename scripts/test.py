ce318628e65a588b349adb4c9f1394fdf2e16565
gabegekas <89178737+gabegekas@users.noreply.github.com>
2024-10-17 16:00:39 -0700

    gitignore

--- scripts/test.py
+++ scripts/test.py
@@ -11,29 +11,31 @@ def plot_temp_vs_depth():
     except:
         print("Importing data...")
         t0 = time.time()
-        ArgoSet = argopy.DataFetcher().region([80, 95, 5, 20, 0, 5, '2020-01', '2020-02']) # Bay of Bengal
+        ArgoSet = argopy.DataFetcher().region([80, 95, 5, 20, 0, 2000, '2020-01', '2020-02']) # Bay of Bengal
         print("Time to create DataFetcher variable : ", time.time() - t0)
         ds = ArgoSet.data.argo.point2profile().to_dataframe()
         print("Time to get data from DataFetcher : ", time.time() - t0)
         ds.to_csv('data.csv')
 
     # Remove rows where temp is None
-    df = df_raw.dropna(subset=['TEMP'])
+    df = df_raw.dropna(subset=['TEMP', 'PRES'])
 
     # Define the variables to work with:
     temp = np.array(df['TEMP'])
-    lat = np.array(df['DEPTH'])
+    pressure = np.array(df['PRES'])
+    print(pressure)
 
     # Scatter data and add grid
-    plt.scatter(dep, temp, s=1)
+    plt.scatter(temp, pressure, s=1)
     plt.grid(True)
+    plt.gca().invert_yaxis()
 
     # Adding labels to the axes and title
-    plt.xlabel('Depth')
-    plt.ylabel('Temperature')
-    plt.title('Temperature Vs. Depth')
+    plt.xlabel('Temperature [C]')
+    plt.ylabel('Pressure [dBar]')
+    plt.title('Temperature Vs. Pressure')
 
-    plt.savefig('figures/temp_vs_depth.png')
+    # plt.savefig('figures/temp_vs_depth.png')
     plt.show()
 
     return True
@@ -41,6 +43,7 @@ def plot_temp_vs_depth():
 
 if __name__ == "__main__":
     flag = False
+    # plot_temp_vs_depth()
     while not flag:
         try:
             flag = plot_temp_vs_depth()
